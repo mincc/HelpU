6ff99fc4ce33b413266e236f140fbbd055f64efa
mincc <bengsnail2002@yahoo.com>
2015-09-12 16:40:54 +0800

    12-09-2015

--- app/src/main/java/com/example/chungmin/helpu/AlarmManagerBroadcastReceiver.java
+++ app/src/main/java/com/example/chungmin/helpu/AlarmManagerBroadcastReceiver.java
@@ -5,99 +5,63 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
+import java.util.List;
+
+/**
+ * Created by Chung Min on 7/19/2015.
+ * 09 Sep 2015 cm.choong : add customerRatingValue, serviceProviderRatingValue, alreadyReadNotification;
+ */
 
 public class AlarmManagerBroadcastReceiver extends BroadcastReceiver {
-    Boolean mIsTrigger = false;
-    Boolean mIsServerAlreadyResponse = true;
-    Boolean mIsAlreadyFinishFullCycle = true;
-    CustomerRequest mCustomerRequest = null;
 
     public AlarmManagerBroadcastReceiver() {
-
     }
 
     @Override
     public void onReceive(final Context context, Intent intent) {
-        mIsTrigger = ((Globals) context.getApplicationContext()).getIsJobOfferTriggle();
-        if (!mIsTrigger) {
-            //only execute when the trigger is false
-            if (mIsServerAlreadyResponse) {
-                if (mIsTrigger) {
-                    return;
-                }
-                checkNeedTriggerNotification(context);
-            }
-        } else {
-            if(mIsAlreadyFinishFullCycle) {
-                mIsAlreadyFinishFullCycle = false;
-                //Retrieve related customer request id and save as global object for view the notification
-                String url = context.getString(R.string.server_uri) + ((Globals) context.getApplicationContext()).getServiceProviderJobOffer();
-                ServiceProviderServerRequests serverRequest = new ServiceProviderServerRequests(context);
-                int userId = ((Globals) context.getApplicationContext()).getUserId();
-                final ProjectStatus[] projectStatus = {null};
-                serverRequest.getServiceProviderJobOffer(userId, url, new GetCustomerRequestCallback() {
-                    @Override
-                    public void done(CustomerRequest returnedCustomerRequestServiceProvider) {
-                        if(returnedCustomerRequestServiceProvider != null) {
-                            ((Globals) context.getApplicationContext()).setCustomerRequest(returnedCustomerRequestServiceProvider);
-                            projectStatus[0] = returnedCustomerRequestServiceProvider.getProjectStatus();
-                            if (projectStatus[0] == ProjectStatus.CandidateNotification) {
-                                Intent serviceIntent = new Intent(context, TriggerNotificationService.class);
-                                serviceIntent.putExtra("projectStatusId", returnedCustomerRequestServiceProvider.getProjectStatusId());
-                                context.startService(serviceIntent);
-                            }
-                        }
-
-                        String url = context.getString(R.string.server_uri) + ((Globals) context.getApplicationContext()).getServiceProviderWinAward();
-                        ServiceProviderServerRequests serverRequest = new ServiceProviderServerRequests(context);
-                        int userId = ((Globals) context.getApplicationContext()).getUserId();
-                        serverRequest.getServiceProviderWinAward(userId, url, new GetCustomerRequestCallback() {
-                            @Override
-                            public void done(CustomerRequest returnedCustomerRequestServiceProvider) {
-                                if(returnedCustomerRequestServiceProvider != null) {
-                                    ((Globals) context.getApplicationContext()).setCustomerRequest(returnedCustomerRequestServiceProvider);
-                                    projectStatus[0] = returnedCustomerRequestServiceProvider.getProjectStatus();
-                                    if (projectStatus[0] == ProjectStatus.WinAwardNotification) {
-                                        Intent serviceIntent = new Intent(context, TriggerNotificationService.class);
-                                        serviceIntent.putExtra("projectStatusId", returnedCustomerRequestServiceProvider.getProjectStatusId());
-                                        context.startService(serviceIntent);
-                                    }
-                                }
-                                mIsAlreadyFinishFullCycle = true;
-                                mIsTrigger = false;
-                                ((Globals) context.getApplicationContext()).setIsJobOfferTriggle(false);
-                                mIsServerAlreadyResponse = false;
-
-                            }
-                        });
-                    }
-                });
-            }
-        }
+        TriggerNotification(context);
     }
 
-    private void checkNeedTriggerNotification(final Context context) {
-        mIsServerAlreadyResponse = false;
-        String url = context.getString(R.string.server_uri) + ((Globals)context.getApplicationContext()).getServiceProviderIsNotificationTrigger();
-        ServiceProviderServerRequests serverRequest = new ServiceProviderServerRequests(context);
+    private void TriggerNotification(final Context context) {
+        String url = context.getString(R.string.server_uri) + ((Globals) context.getApplicationContext()).getCustomerRequestNotificationTrigger();
+        CustomerRequestServerRequests serverRequest = new CustomerRequestServerRequests();
         int userId = ((Globals)context.getApplicationContext()).getUserId();
-        serverRequest.getServiceProviderIsNotificationTrigger(userId, url, new GetBooleanCallback() {
+        serverRequest.getCustomerRequestNotificationTrigger(userId, url, new GetCustomerRequestListCallback() {
             @Override
-            public void done(Boolean isTrigger) {
-                if (isTrigger == null) {
-                    return;
+            public void Complete(List<CustomerRequest> customerRequestList) {
+                for (int i = 0; i < customerRequestList.size(); i++) {
+                    ProjectStatus projectStatus = customerRequestList.get(i).getProjectStatus();
+                    Intent serviceIntent = new Intent(context, TriggerNotificationService.class);
+                    switch (projectStatus) {
+                        case SelectedNotification:
+                        case ConfirmRequestNotification:
+                        case QuotationNotification:
+                        case DealNotification:
+                        case PlanStartDateNotification:
+                        case ServiceStartNotification:
+                        case ServiceDoneNotification:
+                        case CustomerRatingNotification:
+                        case ServiceProviderRatingNotification:
+                        case ConfirmQuotationNotification:
+                            serviceIntent.putExtra("customerRequest", customerRequestList.get(i));
+                            break;
+                        default:
+                            Toast.makeText(context, R.string.strUnknownAction, Toast.LENGTH_LONG).show();
+                            break;
+                    }
+                    context.startService(serviceIntent);
                 }
+            }
 
-                ((Globals) context.getApplicationContext()).setIsJobOfferTriggle(isTrigger);
-                mIsServerAlreadyResponse = true;
-                //
-                return;
+            @Override
+            public void Failure(String msg) {
+
             }
         });
 
     }
-
 
     public void SetAlarm(Context context) {
         AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
