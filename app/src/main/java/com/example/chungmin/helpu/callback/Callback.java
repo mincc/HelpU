7ef11e3a2499c5314bf847ed73ab07ff02acc8a6
mincc <bengsnail2002@yahoo.com>
2015-10-10 09:18:53 +0800

    10-10-2015

---
+++ app/src/main/java/com/example/chungmin/helpu/callback/Callback.java
@@ -0,0 +1,89 @@
+package com.example.chungmin.helpu.callback;
+
+import com.example.chungmin.helpu.models.AppStats;
+import com.example.chungmin.helpu.models.CustomerIssue;
+import com.example.chungmin.helpu.models.CustomerRequest;
+import com.example.chungmin.helpu.models.Rating;
+import com.example.chungmin.helpu.models.ServiceProvider;
+import com.example.chungmin.helpu.models.Transaction;
+import com.example.chungmin.helpu.models.User;
+import com.example.chungmin.helpu.models.UserStats;
+
+import java.util.List;
+
+/**
+ * Created by Chung Min on 9/24/2015.
+ */
+public class Callback {
+    public interface CountCallback {
+        void complete(int data);
+
+        void failure(String msg);
+    }
+
+    public interface GetAppStatsCallback {
+        void complete(AppStats returnedAppStats);
+
+        void failure(String msg);
+    }
+
+    public interface GetBooleanCallback {
+        void complete(Boolean isTrigger);
+
+        void failure(String msg);
+    }
+
+    public interface GetCustomerRequestCallback {
+        void complete(CustomerRequest returnedCustomerRequest);
+
+        void failure(String msg);
+    }
+
+    public interface GetCustomerRequestListCallback {
+        void complete(List<CustomerRequest> data);
+
+        void failure(String msg);
+    }
+
+    public interface GetRatingCallback {
+        void complete(Rating returnedRating);
+
+        void failure(String msg);
+    }
+
+    public interface GetServiceProviderCallback {
+        void complete(ServiceProvider returnedServiceProvider);
+
+        void failure(String msg);
+    }
+
+    public interface GetServiceProviderListCallback {
+        void complete(List<ServiceProvider> returnedServiceProviderList);
+
+        void failure(String msg);
+    }
+
+    public interface GetTransactionCallback {
+        void complete(Transaction returnedTransaction);
+
+        void failure(String msg);
+    }
+
+    public interface GetUserCallback {
+        void complete(User returnedUser);
+
+        void failure(String msg);
+    }
+
+    public interface GetUserStatsCallback {
+        void complete(UserStats returnedUserStats);
+
+        void failure(String msg);
+    }
+
+    public interface GetCustomerIssueCallback {
+        void complete(CustomerIssue returnedCustomerIssue);
+
+        void failure(String msg);
+    }
+}
